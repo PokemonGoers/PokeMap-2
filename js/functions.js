1f7c196eb2606a62dbebe3cf064877ffabd6202d
timoludwig <ti.ludwig@web.de>
2016-08-31 19:52:57 +0200

    solved #14 Timer in popup

--- js/functions.js
+++ js/functions.js
@@ -76,21 +76,18 @@ function abilitiesTitle(abilities) {
 
 function initializeCountdown(id, time) {
     var countdownSpan = document.getElementById(id);
-
     function updateCountdown() {
         var t = Date.parse(time) - Date.parse(new Date());
         var seconds = Math.floor((t / 1000) % 60);
         var minutes = Math.floor((t / 1000 / 60) % 60);
         var hours = Math.floor((t / (1000 * 60* 60)) % 24);
         var days = Math.floor(t / (1000 * 60 * 60 * 24));
-
         countdownSpan.innerHTML = days + " days, " + ('0' + hours).slice(-2) + ":" + ('0' + minutes).slice(-2) + ":" + ('0' + seconds).slice(-2);
-
         if (t <= 0) {
             clearInterval(interval);
+            countdownSpan.innerHTML = "Pokémon already appeared!";
         }
     }
-
     updateCountdown();
     var interval = setInterval(updateCountdown, 1000);
 }
