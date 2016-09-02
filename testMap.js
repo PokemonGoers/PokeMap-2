d72d5e5f36822323f88836ce28565265f8952bf8
Faris Cakaric <cakaricf@gmail.com>
2016-09-02 12:20:42 +0200

    Sidebar html moved to string

--- testMap.js
+++ testMap.js
@@ -1,21 +1,37 @@
 var functions = require('./js/functions');
 var opencyclemap = require('./js/opencyclemap');
 
-var xhr= new XMLHttpRequest();
-xhr.open('GET', 'css/sidebar.html', true);
-xhr.onreadystatechange= function() {
-    if (this.readyState!==4) return;
-    if (this.status!==200) return; // or whatever error handling you want
-    //	document.body.innerHTML += this.responseText;
-    var elemDiv = document.createElement('div');
-	elemDiv.innerHTML = this.responseText;
-	document.body.appendChild(elemDiv);
-    
-};
-xhr.send();
+var sidebarHTML = '<div id="sidebar">\
+		<!--<div id="close"><a href="#" onclick="hideAdditionalInformation()">×</a></div>-->\
+		<div class="avatarhelper"><div id="avatar"></div></div>\
+		<div class="titlehelper"><div id="name">Pokemon Name</div><div class="closehelper" onclick="hideAdditionalInformation()"></div></div>\
+		<h2>Information</h2>\
+		<table>\
+			<tr><td class="labelpok">Evolution:</td><td class="infopok"><span id="evolution"></span></td></tr>\
+			<tr><td class="labelpok">Category:</td><td class="infopok"><span id="category"></span></td></tr>\
+			<tr><td class="labelpok">Type:</td><td class="infopok"><span id="type"></span></td></tr>\
+			<tr><td class="labelpok">Weaknesses:</td><td class="infopok"><span id="weaknesses"></span></td></tr>\
+			<tr><td class="labelpok">Height:</td><td class="infopok"><span id="height"></span></td></tr>\
+			<tr><td class="labelpok">Weight:</td><td class="infopok"><span id="weight"></span></td></tr>\
+		</table>\
+		<h2><span id="abilitiestitle"></span></h2>\
+		<span id="abilities"></span>\
+		<h2>Stats</h2>\
+		<table class="stats">\
+			<tr class="labelstats"><td rowspan="2">HP</td><td colspan="2" style="border: 0;">Attack</td><td colspan="2" style="border: 0;">Defense</td><td rowspan="2">Speed</td></tr>\
+			<tr class="labelstats"><td>normal</td><td>special</td><td>normal</td><td>special</td></tr>\
+			<tr class="labelstats"><td><span id="hp"></span></td><td><span id="attack"></span></td><td><span id="defense"></span></td><td><span id="specialattack"></span></td><td><span id="specialdefense"></span></td><td><span id="speed"></span></td></tr>\
+		</table>\
+	</div>';
+
+
 
 opencyclemap.setUpMap(48.264673,11.671434);
 opencyclemap.setUpLocation(48.264673,11.671434);
 var from = new Date("2016-08-01T00:00:00.000Z");
 var to = new Date("2016-09-01T00:00:00.000Z");
 opencyclemap.loadPokemonData(opencyclemap.setPokemonOnMap, from, to);
+
+var elemDiv = document.createElement('div');
+elemDiv.innerHTML = sidebarHTML;
+document.body.appendChild(elemDiv);
