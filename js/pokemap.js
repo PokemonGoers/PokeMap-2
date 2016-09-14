512b4542ea8d8565940a19522563c82b053b97b1
Faris Cakaric <cakaricf@gmail.com>
2016-09-14 21:35:15 +0200

    Refactoring

--- js/pokemap.js
+++ js/pokemap.js
@@ -18,98 +18,89 @@ var PokeMap = function(htmlElement, filter = {pokemonIds: 0, sightingsSince: 0, 
   this.htmlElement = htmlElement.id;
   apiURL = apiEndPoint;
 
-//   = "http://pokedata.c4e3f8c7.svc.dockerapp.io:65014"
-//  coordinates = {latitude: 48.264673, longitude: 11.671434} , timeRange = {from: -200000, to: 30}, ) {
+  // which pokemons should be shown; if null show all pokemons; otherwise only pokemons with ids in the list
+  this.filterPokemons = null;
 
-  //  this.coordinates = [coordinates.latitude, coordinates.longitude];
-  //  console.log(this.coordinates);
-  //  this.zoomLevel = 5;
-  //  this.sliderFrom = timeRange.from;
-//    this.sliderTo = timeRange.to;
+  this.socket = io.connect('http://localhost:3000');
 
-    // which pokemons should be shown; if null show all pokemons; otherwise only pokemons with ids in the list
-    this.filterPokemons = null;
+  this.markers = [];
+  this.currentOpenPokemon = null;
 
-    this.socket = io.connect('http://localhost:3000');
-
-    this.markers = [];
-    this.currentOpenPokemon = null;
-
-    this.setUpMap();
-    this.filter(filter.pokemonIds, filter.sightingsSince, filter.predictionsUntil);
-    //console.log(mymap.getBounds().getNorthWest(), mymap.getBounds().getSouthEast());
+  this.setUpMap();
+  this.filter(filter.pokemonIds, filter.sightingsSince, filter.predictionsUntil);
+  //console.log(mymap.getBounds().getNorthWest(), mymap.getBounds().getSouthEast());
 }
 
 util.inherits(PokeMap, EventEmitter);
 
 PokeMap.prototype.setUpMap = function() {
-    L.Icon.Default.imagePath = 'node_modules/leaflet/dist/images/';
-    mymap = L.map(this.htmlElement).fitWorld();//.setView(this.coordinates, this.zoomLevel);
-    window.map = mymap; // Set map as a global variable
+  L.Icon.Default.imagePath = 'node_modules/leaflet/dist/images/';
+  mymap = L.map(this.htmlElement).fitWorld();//.setView(this.coordinates, this.zoomLevel);
+  window.map = mymap; // Set map as a global variable
 
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
-        maxZoom: 18,
-        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-            '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-            'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-        id: 'mapbox.streets'
-    }).addTo(mymap);
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+    '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    id: 'mapbox.streets'
+  }).addTo(mymap);
+  
+  L.control.locate().addTo(map);
 
-    L.control.locate().addTo(map);
+  var MyControl = L.Control.extend({
+    options: {
+      position: 'bottomright'
+    },
 
-    var MyControl = L.Control.extend({
-        options: {
-            position: 'bottomright'
-        },
-
-        onAdd: function(map) {
-            var controlContainer = L.DomUtil.create('div', 'leaflet-time-slider-container');
-            var sliderContainer = L.DomUtil.create('div', 'leaflet-time-slider', controlContainer);
-            L.DomUtil.create('div', 'leaflet-time-slider-bar', sliderContainer);
-            var dateContainer = L.DomUtil.create('div', 'leaflet-time-slider-dates', sliderContainer);
-            L.DomUtil.create('div', 'leaflet-time-slider-from', dateContainer);
-            L.DomUtil.create('div', 'leaflet-time-slider-to', dateContainer);
-            var hideContainer = L.DomUtil.create('div', 'leaflet-time-slider-hide-container', sliderContainer);
-            var hideLink = L.DomUtil.create('a', 'leaflet-time-slider-hide-link', hideContainer);
-            L.DomUtil.create('span', 'fa fa-angle-double-right', hideLink);
-            var showContainer = L.DomUtil.create('div', 'leaflet-time-slider-show-container', controlContainer);
-            var showLink = L.DomUtil.create('a', 'leaflet-time-slider-show-link', showContainer);
-            L.DomUtil.create('span', 'fa fa-angle-double-left', showLink);
-            sliderContainer.title = 'change time range';
-            hideContainer.title = 'hide slider';
-            showContainer.title = 'show slider';
-            return controlContainer;
-        }
-    });
-
-    map.addControl(new MyControl());
-    document.getElementsByClassName('leaflet-time-slider-hide-link')[0].onclick = function(e) {
-        document.getElementsByClassName('leaflet-time-slider')[0].style.display = 'none';
-        document.getElementsByClassName('leaflet-time-slider-show-container')[0].style.display = 'block';
+    onAdd: function(map) {
+      var controlContainer = L.DomUtil.create('div', 'leaflet-time-slider-container');
+      var sliderContainer = L.DomUtil.create('div', 'leaflet-time-slider', controlContainer);
+      L.DomUtil.create('div', 'leaflet-time-slider-bar', sliderContainer);
+      var dateContainer = L.DomUtil.create('div', 'leaflet-time-slider-dates', sliderContainer);
+      L.DomUtil.create('div', 'leaflet-time-slider-from', dateContainer);
+      L.DomUtil.create('div', 'leaflet-time-slider-to', dateContainer);
+      var hideContainer = L.DomUtil.create('div', 'leaflet-time-slider-hide-container', sliderContainer);
+      var hideLink = L.DomUtil.create('a', 'leaflet-time-slider-hide-link', hideContainer);
+      L.DomUtil.create('span', 'fa fa-angle-double-right', hideLink);
+      var showContainer = L.DomUtil.create('div', 'leaflet-time-slider-show-container', controlContainer);
+      var showLink = L.DomUtil.create('a', 'leaflet-time-slider-show-link', showContainer);
+      L.DomUtil.create('span', 'fa fa-angle-double-left', showLink);
+      sliderContainer.title = 'change time range';
+      hideContainer.title = 'hide slider';
+      showContainer.title = 'show slider';
+      return controlContainer;
     }
-    document.getElementsByClassName('leaflet-time-slider-show-link')[0].onclick = function(e) {
-            document.getElementsByClassName('leaflet-time-slider-show-container')[0].style.display = 'none';
-            document.getElementsByClassName('leaflet-time-slider')[0].style.display = 'block';
-        }
-        // Emit "move" event when the map is moved
-    mymap.on('move', function(e) {
-        PokeMap.prototype.emitMove(mymap.getCenter(), mymap.getZoom());
-    });
+  });
+
+  map.addControl(new MyControl());
+  document.getElementsByClassName('leaflet-time-slider-hide-link')[0].onclick = function(e) {
+    document.getElementsByClassName('leaflet-time-slider')[0].style.display = 'none';
+    document.getElementsByClassName('leaflet-time-slider-show-container')[0].style.display = 'block';
+  }
+  document.getElementsByClassName('leaflet-time-slider-show-link')[0].onclick = function(e) {
+    document.getElementsByClassName('leaflet-time-slider-show-container')[0].style.display = 'none';
+    document.getElementsByClassName('leaflet-time-slider')[0].style.display = 'block';
+  }
+  // Emit "move" event when the map is moved
+  mymap.on('move', function(e) {
+    PokeMap.prototype.emitMove(mymap.getCenter(), mymap.getZoom());
+  });
 
 
 }
 
 
 PokeMap.prototype.goTo = function(coordinates, zoomLevel) {
-    mymap.panTo(coordinates, zoomLevel);
+  mymap.panTo(coordinates, zoomLevel);
 }
 
 PokeMap.prototype.emitMove = function(coordinates, zoomLevel) {
-    this.emit('move', coordinates, zoomLevel);
+  this.emit('move', coordinates, zoomLevel);
 }
 
 PokeMap.prototype.emitClick = function(pokePOI) {
-    this.emit('click', pokePOI);
+  this.emit('click', pokePOI);
 }
 
 PokeMap.prototype.filter = function(pokemonIds, sightingsSince, predictionsUntil) {
@@ -122,8 +113,8 @@ PokeMap.prototype.filter = function(pokemonIds, sightingsSince, predictionsUntil
     this. showPokemonPredictions(predictionsUntil);
   }
   this.socket.on("connect", function () {
-      console.log("Connected to server, sending geo settings..");
-      socket.emit("settings", {mode: "geo", lat: mymap.getCenter().lat, lon: mymap.getCenter().lon, radius: 5000000});
+    console.log("Connected to server, sending geo settings..");
+    socket.emit("settings", {mode: "geo", lat: mymap.getCenter().lat, lon: mymap.getCenter().lon, radius: 5000000});
   });
 
   this.socket.onmessage = function (event) {
@@ -131,9 +122,9 @@ PokeMap.prototype.filter = function(pokemonIds, sightingsSince, predictionsUntil
     console.log(event);
     var mob = JSON.parse(event.data);
     var mobCircle = L.circle(mob.coordinates, 100, {
-        color: '#808080',
-        fillColor: 'red',
-        fillOpacity: 0.1
+      color: '#808080',
+      fillColor: 'red',
+      fillOpacity: 0.1
     }).addTo(mymap);
     mobCircle.bindPopup("PokeMob detected here! Date: " + mob.date);
     mobCircle.on('click', function(e) { this.emitClick(event.data); });
@@ -143,35 +134,35 @@ PokeMap.prototype.filter = function(pokemonIds, sightingsSince, predictionsUntil
 //PokeMap.prototype.on('move', function(a, b) {console.log(a + " " + b);})
 var pokemonLayer, pokemonMapData;
 setPokemonOnMap = function() {
-    if (mymap == null) return;
+  if (mymap == null) return;
 
-    if (typeof pokemonLayer !== "undefined") {
-        map.removeLayer(pokemonLayer);
+  if (typeof pokemonLayer !== "undefined") {
+    map.removeLayer(pokemonLayer);
+  }
+
+  var pokemonIcon = L.Icon.extend({
+    options: {
+      iconSize: [35, 35]
+    }
+  });
+
+  pokemonLayer = L.geoJson(pokemonMapData, {
+
+    onEachFeature: onEachFeature,
+
+    pointToLayer: function(feature, latlng) {
+      var pokemon = new pokemonIcon({
+        iconUrl: feature.properties.img
+      });
+      var pokname = feature.properties.name;
+      return L.marker(latlng, {
+        icon: pokemon,
+        title: pokname,
+        rinseOnHover: true
+      });
     }
 
-    var pokemonIcon = L.Icon.extend({
-        options: {
-            iconSize: [35, 35]
-        }
-    });
-
-    pokemonLayer = L.geoJson(pokemonMapData, {
-
-        onEachFeature: onEachFeature,
-
-        pointToLayer: function(feature, latlng) {
-            var pokemon = new pokemonIcon({
-                iconUrl: feature.properties.img
-            });
-            var pokname = feature.properties.name;
-            return L.marker(latlng, {
-                icon: pokemon,
-                title: pokname,
-                rinseOnHover: true
-            });
-        }
-
-    }).addTo(map);
+  }).addTo(map);
 }
 
 PokeMap.prototype.showPokemonSightings = function(sightingsSince) {
@@ -267,7 +258,7 @@ function onEachFeature(feature, layer) {
 
 showSideBar = function() {
 
-    PokeMap.prototype.emitClick(pokemonForSidebar);
+  PokeMap.prototype.emitClick(pokemonForSidebar);
 
 }
 
